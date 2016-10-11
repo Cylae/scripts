5aa94a7c2d8413bc4431568a704d3e3b533c2400
Alexandre Calais <alexandre.calais@me.com>
2016-10-12 01:19:13 +0200

    Ca commit sec par lààà

---
+++ script.sh
@@ -0,0 +1,132 @@
+#! /bin/bash
+
+######################################
+# title : SCRIPT                     #
+# description : script for Webstart  #
+# author : Alexandre Calais          #
+# date : 121016                      #
+# version : 1.1                      #
+# usage : sh script.sh               #
+# usage 2 : chmod +x script.sh       #
+#	   ./script.sh               #
+######################################
+
+
+clear
+cat << "draw"
+    ___ _____  _   ___  _____ ___   _    _   ___  ___  ___    ___
+  ,' _//_  _/.' \ / o |/_  _// o |,' \ ,' \ / o |/ _/ / o | ,' _/
+ _\ `.  / / / o //  ,'  / / /  ,'/ o |/ o |/ _,'/ _/ /  ,' _\ `. 
+/___,' /_/ /_n_//_/`_\ /_/ /_/`_\|_,' |_,'/_/  /___//_/`_\/___,' 
+          SCRIPT - WEBSTART - 1.0 - ALEXANDRE CALAIS
+                        EXERCICE SHELL
+		SCRIPT WILL START IN 5 SECONDS
+
+
+draw
+echo "Make a choice"
+echo "1 - Start the script"
+echo "2 - Exit the script"
+echo -n "Your choice > "
+read menu
+if [ $menu = "1" ]
+  then
+  clear
+  echo -n "What is your firstname ? > "
+  read firstname
+  else
+    echo "See you soon ! Visit our website : http://ecole-webstart.com/"
+  exit
+  fi
+
+clear
+echo -n "What is your lastname ? > "
+read lastname
+
+clear
+echo "Okay $firstname $lastname ! Let's do this bro'"
+
+echo "So $firstname, you think you can handle this script ? > "
+read answer_1
+
+clear
+if [ $answer_1 = "yes" ] || [ $answer_1 = "Yes" ]
+  then echo "Yeah ? You're ready ? Let's go $prenom !"
+  else
+  echo "OK... See you !"
+  exit
+  fi
+
+echo "How old are you ? > "
+read age
+clear
+
+if [ $age -ge "18" ]
+  then echo "Woaaaaa !"
+  else
+  echo "You still a kid, please return this computer to Daddy."
+  cat << "draw2"
+                                                                                      
+                                                                                      
+NNNNNNNN        NNNNNNNN     OOOOOOOOO     WWWWWWWW                           WWWWWWWW
+N:::::::N       N::::::N   OO:::::::::OO   W::::::W                           W::::::W
+N::::::::N      N::::::N OO:::::::::::::OO W::::::W                           W::::::W
+N:::::::::N     N::::::NO:::::::OOO:::::::OW::::::W                           W::::::W
+N::::::::::N    N::::::NO::::::O   O::::::O W:::::W           WWWWW           W:::::W 
+N:::::::::::N   N::::::NO:::::O     O:::::O  W:::::W         W:::::W         W:::::W  
+N:::::::N::::N  N::::::NO:::::O     O:::::O   W:::::W       W:::::::W       W:::::W   
+N::::::N N::::N N::::::NO:::::O     O:::::O    W:::::W     W:::::::::W     W:::::W    
+N::::::N  N::::N:::::::NO:::::O     O:::::O     W:::::W   W:::::W:::::W   W:::::W     
+N::::::N   N:::::::::::NO:::::O     O:::::O      W:::::W W:::::W W:::::W W:::::W      
+N::::::N    N::::::::::NO:::::O     O:::::O       W:::::W:::::W   W:::::W:::::W       
+N::::::N     N:::::::::NO::::::O   O::::::O        W:::::::::W     W:::::::::W        
+N::::::N      N::::::::NO:::::::OOO:::::::O         W:::::::W       W:::::::W         
+N::::::N       N:::::::N OO:::::::::::::OO           W:::::W         W:::::W          
+N::::::N        N::::::N   OO:::::::::OO              W:::W           W:::W           
+NNNNNNNN         NNNNNNN     OOOOOOOOO                 WWW             WWW            
+                                                                                      
+                                                                                      
+                                                                                      
+draw2
+  exit
+  fi
+
+echo "Quelle était le troisième code de l'exercice de la semaine passée ?"
+read exercice
+clear
+
+if [ $exercice != "42" ]
+  then echo "Ratééééé !"
+  exit
+  else
+  echo "Bien joué $prenom ! "
+  fi
+
+echo "Un des plus grands provider de serveur s'est fait attaqué la semaine passée, de qui s'agit-il ?"
+read OVH
+clear
+
+if [ $OVH = "OVH" ] || [ $OVH = "ovh" ]
+  then echo "Eh oui ! Le bougre s'est prit une attaque d'1Tbps ! Incroyable non ?"
+  else
+  echo "Non ! Non mais tu suis l'actualité ou quoi ?"
+  fi
+
+sleep 2
+clear
+sleep 5
+echo "5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+echo "0"
+sleep 1
+echo "DESTRUCTION IMMINENTE"
+sleep 1
+clear
